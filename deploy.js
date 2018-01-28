5b6c634d3d2fbde48d2dc4be41c2f2bcf7e9b032
Moaw Solutions <hola@moaw.co>
2018-01-28 22:13:46 +0100

    deploy

---
+++ deploy.js
@@ -0,0 +1,35 @@
+var copy = require('directory-copy')
+copy(
+    {
+        src: 'dist'
+        , dest:  './../adangnzlz.github.io'
+        , excludes: [/^\./] // Exclude hidden files 
+    }
+    , function () {
+        console.log('done!')
+        var cmd = require('node-cmd');
+        cmd.get(
+            'git add *',
+            function (err, data, stderr) {
+                cmd.get(
+                    'git commit -m  "deploy"',
+                    function (err, data, stderr) {
+                        cmd.get(
+                            'git push',
+                            function (err, data, stderr) {
+                                console.log('pushed all', data)
+                            }
+                        );
+                    }
+                );
+            }
+        );
+
+
+        
+    })
+    .on('log', function (msg, level) {
+        // Level is debug, info, warn or error 
+        console.log(level + ': ' + msg)
+    })
+
